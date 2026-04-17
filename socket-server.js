fa6cecc053354f24b4f464454b964866a65871a8
Charlie1719 <A01640751@tec.mx>
2026-04-16 20:34:25 -0600

    pepe

--- socket-server.js
+++ socket-server.js
@@ -1,11 +1,6 @@
 import { WebSocketServer } from 'ws';
 
-const PORT = process.env.PORT || 3000;
-const wss = new WebSocketServer({ port: PORT });
-
-wss.on('listening', () => {
-    console.log('Servidor de WebSockets escuchando en el puerto ${PORT}');
-});
+const wss = new WebSocketServer({ port: 3000 });
 
 wss.on('connection', (ws) => {
     console.log('Cliente conectado');
